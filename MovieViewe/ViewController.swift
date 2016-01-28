508ba7eb44d4151990abe774c9138a0c8c33bc50
jeremycardona <jeremy.cardona@upr.edu>
2016-01-28 18:16:23 -0400

    Initial Commit

---
+++ MovieViewe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieViewe
+//
+//  Created by Gabriela A. Renta on 1/28/16.
+//  Copyright © 2016 Jeremy Cardona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
