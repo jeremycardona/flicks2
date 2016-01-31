63253f7aba0b1f219e8e02b946b987bee75a8b58
jeremycardona <jeremy.cardona@upr.edu>
2016-01-31 15:18:41 -0400

    first commit

---
+++ MovieViewe/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  MovieViewe
+//
+//  Created by Gabriela A. Renta on 1/28/16.
+//  Copyright © 2016 Jeremy Cardona. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
